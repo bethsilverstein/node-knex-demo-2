1e22505b41e462c706eb01019467d5a708efcd82
bethsilverstein <beth.silverstein@tradeshift.com>
2022-04-12 12:34:50 -0500

    Add screen.js file and experiment with prettyjson package for formatting

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
-var cfg = require("./knex-cfg").pg;
-
+var cfg = require("./knex-cfg").sqlite; //change .pg to .sqlite if wanted
+var screen = require("./screen");
 var knex = require("knex")(cfg);
 
 //clear();
+screen.clear()
 
-knex.select("firstname", "lastname").from("author").asCallback(function(err, rows) {
-    if (err) {
-        console.error(err);
-    }
-    else {
-        console.log(rows);
-    }
-    knex.destroy();
-    console.log('done with author query');
-})
+
+//this was me testing stuff
+// knex.select("firstname", "lastname").from("author").asCallback(function(err, rows) {
+//     if (err) {
+//         console.error(err);
+//     }
+//     else {
+//         console.log(rows);
+//     }
+//     knex.destroy();
+//     console.log('done with author query');
+// })
 
 
 knex.select("title", "rating").from("book").asCallback(function(err, rows) {
@@ -21,7 +24,9 @@ knex.select("title", "rating").from("book").asCallback(function(err, rows) {
         console.error(err);
     }
     else {
-        console.log(rows);
+        //console.log(rows);
+        //screen.write(rows, "json");
+        screen.write(rows, "pretty");
     }
     knex.destroy();
     console.log('done with book query');
@@ -30,9 +35,9 @@ knex.select("title", "rating").from("book").asCallback(function(err, rows) {
 //knex.destroy();
 //console.log('doneeee');
 
-function clear ()
-{
-    process.stdout.write("\033c");
-}
+// function clear ()
+// {
+//     process.stdout.write("\033c");
+// }
 
 
